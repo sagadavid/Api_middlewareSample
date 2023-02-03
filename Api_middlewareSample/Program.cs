cd30aae29228ec25d2055f6695479480820f1034
sagadavid <epostsaga@gmail.com>
2023-02-04 00:24:00 +0100

    TERMINAL MIDDLEWARE

--- Api_middlewareSample/Program.cs
+++ Api_middlewareSample/Program.cs
@@ -12,7 +12,17 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-//app.Run();
+app.Run(async context =>
+{
+    await context.Response.WriteAsync("this is middleware component");
+    //We use the Run method, which adds a terminal component
+    //to the app pipeline. We can see we are not using the next
+    //delegate because the Run method is always terminal and
+    //terminates the pipeline. This method accepts a single
+    //parameter of the RequestDelegate type.
+
+}
+);
 
 app.MapControllers();
 
